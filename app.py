a98234a0e621eb8386ffd84fbb66f098573277bf
Neranjaka Jayarathne <neranjaka@yahoo.com>
2021-09-17 09:33:28 -0400

    Trying Heroku

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import requests
-r = requests.get('https://github.com/neranjaka', auth=('user', 'pass'))
+#r = requests.get('https://github.com/neranjaka', auth=('user', 'pass'))
 
 
-print(r.status_code)
+#print(r.status_code)
 
-print(r.headers['content-type'])
+#print(r.headers['content-type'])
 
-print(r.encoding)
+#print(r.encoding)
 
 #print(r.text)
 
 
-print(r.json())
+#print(r.json())
+
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=1RKMGQVQE9C9S5RN'
+r = requests.get(url)
+data = r.json()
+
+print(data)
